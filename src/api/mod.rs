b6b8848985410d3f685ec5157d64b4fc6ea738d4
Luc Georges <luc.sydney.georges@gmail.com>
2023-07-31 16:39:41 +0200

    feat(api): add lfs metadata

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -7,11 +7,20 @@ pub mod tokio;
 /// The synchronous version of the API
 pub mod sync;
 
+/// Lfs metadata of a file
+#[derive(Debug, Clone, Deserialize, PartialEq)]
+pub struct Lfs {
+    /// The file size in bytes.
+    pub size: u32,
+}
+
 /// Siblings are simplified file descriptions of remote files on the hub
 #[derive(Debug, Clone, Deserialize, PartialEq)]
 pub struct Siblings {
     /// The path within the repo.
     pub rfilename: String,
+    /// Lfs metadata
+    pub lfs: Option<Lfs>,
 }
 
 /// The description of the repo given by the hub
