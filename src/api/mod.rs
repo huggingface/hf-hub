0ad26cbc367611b9b51b88331f52945edbcde3e3
Luc Georges <luc@huggingface.co>
2023-08-01 10:59:54 +0200

    refactor(api): remove unused commented code

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -7,27 +7,11 @@ pub mod tokio;
 /// The synchronous version of the API
 pub mod sync;
 
-// /// Lfs metadata of a file
-// #[derive(Debug, Clone, Deserialize, PartialEq)]
-// pub struct Lfs {
-//     /// The file size in bytes.
-//     pub size: u32,
-// }
-
 /// Siblings are simplified file descriptions of remote files on the hub
 #[derive(Debug, Clone, Deserialize, PartialEq)]
 pub struct Siblings {
     /// The path within the repo.
     pub rfilename: String,
-    // /// Git blob id
-    // #[serde(rename = "blobId")]
-    // pub blob_id: String,
-    // /// The file size in bytes.
-    // /// Note that this can be the LFS pointer size on some occasions
-    // /// and not the actual file size. See `lfs`.
-    // pub size: u32,
-    // /// Lfs metadata
-    // pub lfs: Option<Lfs>,
 }
 
 /// The description of the repo given by the hub
