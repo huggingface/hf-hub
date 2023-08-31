c160c22becc599a1080ea6cdb1a12ab7127f9bce
Evgeny Igumnov <igumnovnsk@gmail.com>
2023-08-31 20:24:32 +0600

    refactoring - remove copy/paste code

--- src/api/sync.rs
+++ src/api/sync.rs
@@ -12,9 +12,10 @@ use std::collections::HashMap;
 // };
 use super::RepoInfo;
 use std::num::ParseIntError;
-use std::path::{Component, Path, PathBuf};
+use std::path::{PathBuf};
 use thiserror::Error;
 use ureq::{Agent, Request};
+use crate::api::universal::Universal;
 
 /// Current version (used in user-agent)
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -200,57 +201,6 @@ pub struct Api {
     progress: bool,
 }
 
-fn make_relative(src: &Path, dst: &Path) -> PathBuf {
-    let path = src;
-    let base = dst;
-
-    if path.is_absolute() != base.is_absolute() {
-        panic!("This function is made to look at absolute paths only");
-    }
-    let mut ita = path.components();
-    let mut itb = base.components();
-
-    loop {
-        match (ita.next(), itb.next()) {
-            (Some(a), Some(b)) if a == b => (),
-            (some_a, _) => {
-                // Ignoring b, because 1 component is the filename
-                // for which we don't need to go back up for relative
-                // filename to work.
-                let mut new_path = PathBuf::new();
-                for _ in itb {
-                    new_path.push(Component::ParentDir);
-                }
-                if let Some(a) = some_a {
-                    new_path.push(a);
-                    for comp in ita {
-                        new_path.push(comp);
-                    }
-                }
-                return new_path;
-            }
-        }
-    }
-}
-
-fn symlink_or_rename(src: &Path, dst: &Path) -> Result<(), std::io::Error> {
-    if dst.exists() {
-        return Ok(());
-    }
-
-    let rel_src = make_relative(src, dst);
-    #[cfg(target_os = "windows")]
-    {
-        if std::os::windows::fs::symlink_file(rel_src, dst).is_err() {
-            std::fs::rename(src, dst)?;
-        }
-    }
-
-    #[cfg(target_family = "unix")]
-    std::os::unix::fs::symlink(rel_src, dst)?;
-
-    Ok(())
-}
 
 impl Api {
     /// Creates a default Api, for Api options See [`ApiBuilder`]
@@ -480,7 +430,7 @@ impl ApiRepo {
         pointer_path.push(filename);
         std::fs::create_dir_all(pointer_path.parent().unwrap()).ok();
 
-        symlink_or_rename(&blob_path, &pointer_path)?;
+        Universal::symlink_or_rename(&blob_path, &pointer_path)?;
         self.api
             .cache
             .repo(self.repo.clone())
