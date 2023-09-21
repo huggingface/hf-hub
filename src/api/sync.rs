6d8d8591bdbbd5675dcb1cc14cc28f68d418250b
Benedikt Schaber <benedikt.schaber@protonmail.com>
2023-09-21 14:13:38 +0200

    Use stream position when continuing download and remove ResumableReader

--- src/api/sync.rs
+++ src/api/sync.rs
@@ -2,6 +2,7 @@ use crate::{Cache, Repo, RepoType};
 use indicatif::{ProgressBar, ProgressStyle};
 use rand::Rng;
 use std::collections::HashMap;
+use std::io::Seek;
 // use reqwest::{
 //     blocking::Agent,
 //     header::{
@@ -16,8 +17,6 @@ use std::num::ParseIntError;
 use std::path::{Component, Path, PathBuf};
 use thiserror::Error;
 use ureq::{Agent, Request};
-
-mod reader;
 
 /// Current version (used in user-agent)
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -352,13 +351,12 @@ impl Api {
 
         if self.max_retries > 0 {
             let mut i = 0;
-            let mut current = 0;
-            let mut res = self.download_from(url, &mut current, &mut file);
+            let mut res = self.download_from(url, 0u64, &mut file);
             while let Err(dlerr) = res {
                 let wait_time = exponential_backoff(300, i, 10_000);
                 std::thread::sleep(std::time::Duration::from_millis(wait_time as u64));
 
-                res = self.download_from(url, &mut current, &mut file);
+                res = self.download_from(url, file.stream_position()?, &mut file);
                 i += 1;
                 if i > self.max_retries {
                     return Err(ApiError::TooManyRetries(dlerr.into()));
@@ -389,7 +387,7 @@ impl Api {
     fn download_from(
         &self,
         url: &str,
-        current: &mut usize,
+        current: u64,
         file: &mut std::fs::File,
     ) -> Result<(), ApiError> {
         let range = format!("bytes={current}-");
@@ -399,8 +397,7 @@ impl Api {
             .set(RANGE, &range)
             .call()
             .map_err(Box::new)?;
-        let reader = response.into_reader();
-        let mut reader = reader::ResumableReader::new(reader, current);
+        let mut reader = response.into_reader();
         std::io::copy(&mut reader, file)?;
         Ok(())
     }
